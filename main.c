6e5bb6f6090eede4c3a83074cd491bc0979401cf
Mogullapally Sudheekshna <134086536+Sudheekshna-M@users.noreply.github.com>
2023-05-19 23:43:38 +0530

    Create main.c

---
+++ main.c
@@ -0,0 +1,89 @@
+#include <pic18f4550.h>
+#include <stdio.h>
+#include <math.h>
+#define MINTHR 8000
+#define RESOLUTION 488
+#define InternalOsc_8MHz 8000000
+#define InternalOsc_4MHz 4000000
+#define InternalOsc_2MHz 2000000
+#define InternalOsc_1MHz 1000000
+#define InternalOsc_500KHz 500000
+#define InternalOsc_250KHz 250000
+#define InternalOsc_125KHz 125000
+#define InternalOsc_31KHz 31000
+#define Timer2Prescale_1 1
+#define Timer2Prescale_4 4
+#define Timer2Prescale_16 16
+#define mybit PORTBbits.RB4
+ /* define for setting direction */
+void PWM_Init() /* Initialize PWM */
+{
+ TRISCbits.TRISC2 = 0; /* Set CCP1 pin as output for PWM out */
+ 4
+ CCP1CON = 0x0C; /* Set PWM mode */
+}
+int setPeriodTo(unsigned long FPWM)/* Set period */
+{
+ int clockSelectBits, TimerPrescaleBits;
+ int TimerPrescaleValue;
+ float period;
+ unsigned long FOSC, _resolution = RESOLUTION;
+ if (FPWM < MINTHR) {TimerPrescaleBits = 2; TimerPrescaleValue = Timer2Prescale_16;}
+ else {TimerPrescaleBits = 0; TimerPrescaleValue = Timer2Prescale_1;}
+ if (FPWM > _resolution) {clockSelectBits = 7; FOSC = InternalOsc_8MHz;}
+ else if (FPWM > (_resolution >>= 1)) {clockSelectBits = 6; FOSC = InternalOsc_4MHz;}
+ else if (FPWM > (_resolution >>= 1)) {clockSelectBits = 5; FOSC = InternalOsc_2MHz;}
+ else if (FPWM > (_resolution >>= 1)) {clockSelectBits = 4; FOSC = InternalOsc_1MHz;}
+ else if (FPWM > (_resolution >>= 1)) {clockSelectBits = 3; FOSC = InternalOsc_500KHz;}
+ else if (FPWM > (_resolution >>= 1)) {clockSelectBits = 2; FOSC = InternalOsc_250KHz;}
+ else if (FPWM > (_resolution >>= 1)) {clockSelectBits = 1; FOSC = InternalOsc_125KHz;}
+ else {clockSelectBits = 0; FOSC = InternalOsc_31KHz;}
+ period = ((float)FOSC / (4.0 * (float)TimerPrescaleValue * (float)FPWM)) - 1.0;
+ period = round(period);
+ OSCCON = ((clockSelectBits & 0x07) << 4) | 0x02;
+ PR2 = (int)period;
+ T2CON = TimerPrescaleBits;
+ TMR2 = 0;
+ T2CONbits.TMR2ON = 1; /* Turn ON Timer2 */
+ 5
+ return (int)period;
+}
+void SetDutyCycleTo(float Duty_cycle, int Period)
+{
+ int PWM10BitValue;
+ PWM10BitValue = 4.0 * ((float)Period + 1.0) * (Duty_cycle/100.0);
+ CCPR1L = (PWM10BitValue >> 2);
+ CCP1CON = ((PWM10BitValue & 0x03) << 4) | 0x0C;
+}
+void delay(unsigned int val)
+{
+ unsigned int i,j;
+ for(i=0;i<val;i++)
+ for(j=0;j<10;j++);
+}
+int main() 
+{
+ int Period;
+ PWM_Init(); /* Initialize PWM */
+ Period = setPeriodTo(50); /* 50Hz PWM frequency */
+ /* Note that period step size will gradually increase with PWM frequency */
+ 
+ TRISBbits.TRISB4 = 1 ; 
+ while(1){
+ 
+ 6
+ if(mybit==1){
+ 
+ delay(3000);
+ SetDutyCycleTo(12.0, Period); /* 12% duty cycle */
+ 
+ }
+ 
+ else{
+ delay(3000);
+ SetDutyCycleTo(7.6, Period); /* 3% duty cycle */
+ 
+ }
+ 
+ }
+}
